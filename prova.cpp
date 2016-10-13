793b58920f9892c52ec3ae6e956e8f5f714377fc
Víctor Campello <Victor@MacBook-Pro-de-Victor.local>
2016-10-13 19:08:55 +0200

    Victor modificant

--- prova.cpp
+++ prova.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <cstdlib>
+include <cstdlib>
 #include <string>
 int main(){
   double z_min, z_max, z_step, z;
   int z_index;
 
-  z_max = 3.5;
+  z_max = 3.7;
   z_min = 2.0;
   z_step = 0.000428571;
   z = 2.59325;
